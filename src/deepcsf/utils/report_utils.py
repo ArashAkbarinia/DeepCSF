9f2935bec3e31d7bbe2f23cc54a63bd5061ee99a
Arash Akbarinia <akbarinia.arash@gmail.com>
2022-01-04 17:09:58 +0100

    accuracy for fc=1

--- src/deepcsf/utils/report_utils.py
+++ src/deepcsf/utils/report_utils.py
@@ -49,6 +49,10 @@ def accuracy_preds(output, target, topk=(1,)):
 
 
 def accuracy(output, target, topk=(1,)):
+    if output.shape[1] == 1:
+        acc = (output > 0) == target
+        acc = [acc.float().mean(0, keepdim=True)[0]]
+        return acc
     res, _ = accuracy_preds(output, target, topk=topk)
     return res
 
